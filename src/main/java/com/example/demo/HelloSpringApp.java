6b5df479a0cfe71c2669c3b0907e5663983085fc
marusosasssi <soassi@outlook.com>
2022-12-13 08:26:38 -0600

    creation of the spring container and retrieving the beans from the container

--- src/main/java/com/example/demo/HelloSpringApp.java
+++ src/main/java/com/example/demo/HelloSpringApp.java
@@ -1,2 +1,19 @@
-package com.example.demo;public class HelloSpringApp {
+package com.example.demo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class HelloSpringApp {
+    public static void main(String[] args) {
+        //load the Spring configuration file
+        ClassPathXmlApplicationContext context =
+            new ClassPathXmlApplicationContext("applicationContext.xml");
+        //retrieve bean from spring container
+        Coach theCoach = context.getBean("myCoach", Coach.class);
+        //(beanId, interface) = ("myCoach, Coach.class);
+
+        //call methods on the bean
+        System.out.println(theCoach.getDailyWorkout());
+        //close the context
+        context.close();
+    }
 }
