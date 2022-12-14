9369bdf2ec00e7da64e7e5007cc20ed390eab232
marusosasssi <soassi@outlook.com>
2022-12-13 22:24:48 -0600

    injecting literal values in the container

--- src/main/java/com/example/demo/SetterDemoApp.java
+++ src/main/java/com/example/demo/SetterDemoApp.java
@@ -15,6 +15,10 @@ public class SetterDemoApp {
         System.out.println(coach.getDailyWorkout());
         System.out.println(coach.getDailyFortune());
 
+        //call our new methods to get the literal values
+        System.out.println(coach.getEmailAddress());
+        System.out.println(coach.getTeam());
+
         //close the context
         context.close();
 
