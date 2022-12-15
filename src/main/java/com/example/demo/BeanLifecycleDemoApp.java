89d4ceeeed2ad36c7f302caf4615507911d924e0
marusosasssi <soassi@outlook.com>
2022-12-14 21:28:19 -0600

    bean lifecycle: configuring method names in spring config file

--- src/main/java/com/example/demo/BeanLifecycleDemoApp.java
+++ src/main/java/com/example/demo/BeanLifecycleDemoApp.java
@@ -2,23 +2,16 @@ package com.example.demo;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class BeanScopeDemoApp {
+public class BeanLifecycleDemoApp {
     public static void main(String[] args) {
         //load the spring configuration file
         ClassPathXmlApplicationContext context =
-            new ClassPathXmlApplicationContext("beanScope-applicationContext.xml");
+            new ClassPathXmlApplicationContext("beanLifeCycle-applicationContext.xml");
 
         //retrieve bean from spring container
         Coach theCoach = context.getBean("myCoach", Coach.class);
 
-        Coach alphaCoach = context.getBean("myCoach", Coach.class);
-
-        boolean result = (theCoach == alphaCoach);
-
-        System.out.println("\nPointing to the same object? " + result);
-
-        System.out.println("\nMemory location for theCoach: " + theCoach);
-        System.out.println("\nMemory location for alphaCoach: " + alphaCoach);
+        System.out.println(theCoach.getDailyWorkout());
 
         // close the context
         context.close();
